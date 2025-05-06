1a9996864023e531ce74b94377b6d54ae77e3a37
armor1206 <munkhnogoontaiga@gmail.com>
2025-05-06 13:13:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def upload_file():
     return jsonify({"message": "File uploaded", "filename": filename})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
